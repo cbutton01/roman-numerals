5c963be0dd14c8927d1acb5e00d22b7a2be5f41b
Cameron Button and jared lutz <cameron@>
2018-03-29 10:39:47 -0700

    add function for multiple of 1, 10, 100, and 1000

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,42 @@ function numerals (input){
     return 'N/A';
   }
 }
+
+
+function numeralIValue (number) {
+  var finalOutput = "";
+  for (var i = 1; i <= number ; i++) {
+    finalOutput += "I";
+  }
+  return finalOutput;
+}
+
+function numeralXValue (number){
+  debugger;
+  var finalOutput = "";
+  numberOfX = number / 10;
+  for (var i = 1; i <= numberOfX; i++) {
+    finalOutput += "X";
+  }
+
+  return finalOutput;
+}
+
+function numeralCValue (number){
+  var finalOutput = "";
+  numberOfC = number / 100;
+  for (var i = 1; i <= numberOfC; i++) {
+    finalOutput += "C";
+  }
+
+  return finalOutput;
+}
+
+function numeralMValue (number){
+  var finalOutput = "";
+  numberOfM = number / 1000;
+  for (var i = 1; i <= numberOfM; i++) {
+    finalOutput += "M";
+  }
+  return finalOutput;
+}
